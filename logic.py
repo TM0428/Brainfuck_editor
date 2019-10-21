602cbcdc53518f25244615a96e4dadba575ef2bf
TM <>
2019-10-21 16:10:32 +0900

    input用のコードを出力する関数を追加

--- logic.py
+++ logic.py
@@ -1,7 +1,25 @@
-def int_input(len,piv,input_dec):
+import main
+
+def int_input(bf,input_dec):
     """
-    len is variable length
     piv is the number Memory address to point
     input_dec is Sequence number of input destination
     """
-    
+    output = ""
+    bf.len = bf.len * 2
+    input_dec = input_dec * 2
+    if bf.len > bf.piv:
+        for i in range(bf.len-bf.piv):
+            output += '>'
+    elif bf.len < bf.piv:
+        for i in range(bf.len-bf.piv):
+            output += '<'
+    output += ">+[[-]>,----------[<++++[>-----<-]>--[<++++[>----<-]>>[<++++++++++>-]<[>+<-]<+>]]<]>>[<<<+>>>-]<<<["
+    for i in range(bf.len - input_dec):
+        output += '<'
+    output += '+'
+    for i in range(bf.len - input_dec):
+        output += '>'
+    output += '-]'
+    bf.piv = input_dec
+    return output
